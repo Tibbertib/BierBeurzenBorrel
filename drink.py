ec843d6184be19b6722c490c654d8c78fe2f5683
Max de Boer-Blazdell <m.deboer-blazdell@ru.nl>
2024-02-12 15:20:03 +0100

    add additional functionalities to drinks.py

--- drink.py
+++ drink.py
@@ -5,7 +5,7 @@ class Drink:
 
     TODO: implement functionality for crashing the price
     """
-    def __init__(self, min_price: int, max_price: int, starting_price: int, nr_drinks: int, name: str):
+    def __init__(self, min_price: int, max_price: int, starting_price: int, nr_drinks: int, name: str) -> None:
         # Keep in mind that prices are defined in cents to ensure there is no fuckery going on with floats and to keep calculations consistent
         self.min_price = min_price
         self.max_price = max_price
@@ -14,6 +14,7 @@ class Drink:
         self.nr_drinks = nr_drinks
         self.name = name
         self.for_sale = True
+        self.historic_price = [starting_price]
 
     def modify_price(self, is_sold: bool, price_change: float, drinks_sold: int, Drink=None) -> None:
         # Because this function is used to both increase and decrease the price of Drinks, we must check whether the product was sold in the last transaction
@@ -22,9 +23,16 @@ class Drink:
             if self.nr_drinks <= 0:
                 self.for_sale = False
             self.current_price += price_change
+            self.historic_price.append(self.current_price)
         else:
             self.current_price -= price_change
+            self.historic_price.append(self.current_price)
     
     def can_sell_amount(self, amount: int) -> bool:
         return self.nr_drinks - amount >= 0
-        
+    
+    def reset(self) -> None:
+        self.current_price = self.starting_price
+
+    def increase_drinks_nr(self, amount: int) -> None:
+        self.nr_drinks += amount
